173ff28787261b81becdcb3b42dd14a0662c3c09
Ovoda <cdetourrispro@gmail.com>
2020-04-02 15:01:02 -0700

    deleted JS exercies for security reasons

--- javascript/javascript-exercises/findTheOldest/findTheOldest.js
+++
@@ -1,8 +0,0 @@
-let findTheOldest = function(array) {
-    var age = array.map(x => parseInt((typeof x.yearOfDeath === 'undefined') ? 2020 - x.yearOfBirth : x.yearOfDeath - x.yearOfBirth));
-    var oldest = Math.max.apply(null, age);
-    var person = array[age.indexOf(oldest)];
-    return person;
-}
-
-module.exports = findTheOldest
--- javascript/javascript-exercises/getTheTitles/getTheTitles.js
+++
@@ -1,5 +0,0 @@
-const getTheTitles = function(array) {
-    return array.map(x => x.title);
-}
-
-module.exports = getTheTitles;
